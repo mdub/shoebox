d9551d501d418f62ce6f67f5e14af04bfd70aefa
Mike Williams <mdub@dogbiscuit.org>
2009-02-05 23:01:08 +1100

    Spike uploads.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,25 @@
 class PhotosController < ApplicationController
-  
+
   def index
     @photos = Photo.find(:all)
   end
-  
+
+  def new
+    @photo = Photo.new
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(params[:photo])
+    if @photo.save
+      flash[:notice] = 'Photo saved.'
+      redirect_to photos_path
+    else
+      render :action => :new
+    end
+  end
+
 end
