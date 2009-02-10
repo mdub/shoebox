0d7f9fef87d2e8b64f634602971fdc6b52230d59
Mike Williams <mdub@dogbiscuit.org>
2009-02-10 20:49:31 +1100

    Get the tests working again.

--- spec/views/photos/index.html.erb_spec.rb
+++ spec/views/photos/index.html.erb_spec.rb
@@ -1,9 +1,24 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "photos/index" do
-  
+
   describe "with 3 photos" do
-    assigns[:photos] = (1..3).map { mock_model(Photo) }
+
+    before do
+      assigns[:photos] = [
+        mock_model(Photo, :public_filename => "/photos/a"),
+        mock_model(Photo, :public_filename => "/photos/b"),
+        mock_model(Photo, :public_filename => "/photos/c")
+      ]
+    end
+
+    it "should include links to each photo" do
+      render :action => "photos/index"
+      %w(a b c).each do |photo_id|
+        response.should have_tag("a", :href => "/photos/#{photo_id}")
+      end
+    end
+
   end
-  
+
 end
