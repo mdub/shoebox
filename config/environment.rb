f82ac54a658692a7f5285a65ca0fdf91356bb799
Mike Williams <mdub@dogbiscuit.org>
2009-02-11 22:50:07 +1100

    Add ImageScience.

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
+  config.gem "image_science", :version => "1.1.3"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
