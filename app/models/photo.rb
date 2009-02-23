b23a639f108edec8391eb575f907c6868aef04c7
Mike Williams <mdub@dogbiscuit.org>
2009-02-23 22:34:12 +1100

    Increase the size limit.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,7 @@ class Photo < ActiveRecord::Base
   has_attachment(
   {
     :content_type => :image, 
-    :max_size => 2.megabytes,
+    :max_size => 4.megabytes,
     :storage => :file_system, 
     :path_prefix => "public/var/#{Rails.env}/photos"
   }
