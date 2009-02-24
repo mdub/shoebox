62c56efbdf506074d2768b70bb46a0853e5c679a
Mike Williams <mdub@dogbiscuit.org>
2009-02-24 22:09:39 +1100

    Add exifr for EXIF processing.

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,8 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "image_science", :version => "1.1.3"
   config.gem "mislav-will_paginate", :version => "2.3.6", :lib => "will_paginate", :source => "http://gems.github.com"
-
+  config.gem "remvee-exifr", :lib => "exifr", :source => "http://gems.github.com"
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
