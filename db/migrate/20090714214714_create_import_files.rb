c07b0bd09e2e9b8d84ba444554ce057635da238e
Mike Williams <mdub@dogbiscuit.org>
2009-07-16 09:06:08 +1000

    An ImportFile needs an Import and a path.

--- db/migrate/20090714214714_create_import_files.rb
+++ db/migrate/20090714214714_create_import_files.rb
@@ -2,8 +2,8 @@ class CreateImportFiles < ActiveRecord::Migration
   
   def self.up
     create_table "import_files" do |t|
-      t.integer "import_id"
-      t.string "path"
+      t.integer "import_id", :null => false
+      t.string "path", :null => false
       t.text "message"
       t.integer "photo_id"
       t.datetime "completed_at"
